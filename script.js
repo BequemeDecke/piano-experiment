9afa1c7464844d813266362b68f6bbcb3680e913
EntspannterHase <lukas@exaaim.com>
2025-04-24 08:11:26 +0200

    Create html skeleton

--- script.js
+++ script.js
@@ -1,11 +1,15 @@
 let canvas = document.getElementById("canvas");
-canvas.width = 100;
-canvas.height = 100;
+canvas.width = 540;
+canvas.height = 500;
 
 let context = canvas.getContext("2d");
 
-context.fillStyle = "black";
-context.fillRect(0, 0, canvas.width, canvas.height);
+function createNode(note, number, width=100, height=200) {
+    context.fillStyle = "black";
+    context.fillRect((width + 10) * number, canvas.height - height, width, height);
+}
 
-context.fillStyle = "red";
-context.fillRect(0, 0, 20, 20);
+// context.fillStyle = "red";
+// context.fillRect(0, 0, 20, 20);
+
+Array.from({length: 5}, (_,i) => createNode(null, i))
